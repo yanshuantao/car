763b9126ce23574e72becf560721f840eab0120c
yanshuantao <447268487@qq.com>
2018-06-28 15:20:17 +0800

    111

--- car/car-common/src/main/java/com/yst/common/pojo/T3.java
+++ car/car-common/src/main/java/com/yst/common/pojo/T3.java
@@ -4,7 +4,7 @@ public class T3 {
 	public T3(){
 		System.out.println("I am T3");
 		System.out.println("应该是push设置成main分支就可以了");
-		System.out.println("切换默认分支");
+		System.out.println("切换默认分支"); 
 	}
 
 }
